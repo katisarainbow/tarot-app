5ea4101a211934c351fee010f0141411f463d5ec
Kat <katietesara@gmail.com>
2021-11-17 16:39:30 -0400

    ooox2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Cards from "./components/cards";
-import Categories from "./components/categories";
+import Cards from "./components/Cards";
+import Categories from "./components/Categories";
 
 function App() {
 	const [cards, setCards] = useState();
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,9 @@
 
 const Categories = () => {
-  const Allcat =  ['All', 'The Greater Arcana', 'The Lesser Arcana']; 
+  const allCategories =  ['All', 'The Greater Arcana', 'The Lesser Arcana']; 
 	return(
 		<div>
-		 {Allcat.map((category, index) => {
+		 {allCategories.map((category, index) => {
 			return (
 			<button className='btn-cat' key={index}>
 			{category}		
